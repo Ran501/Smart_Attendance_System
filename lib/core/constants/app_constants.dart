1ee39fa00fbd8851a1edfd2fc71e6b6d8d63b70e
Ran501 <rangdelchoney618@gmail.com>
2026-05-15 22:27:03 +0600

    Changes for the backend

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -2,7 +2,7 @@ class AppConstants {
   static const String appName = 'Smart Attendance';
   static const String apiBaseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://10.0.2.2:3000/api/v1',
+    defaultValue: 'http://192.168.137.138:3000/api/v1',
   );
   static const double faceMatchThreshold = 0.85;
   static const int defaultSessionDurationMinutes = 5;
